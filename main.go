d8956075cbb3a9c555a654a89da8e411250e3381
posinski <piotr.osinski@adpilot.pl>
2015-05-25 10:12:00 +0200

    worker

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"bufio"
 	"strings"
 	"strconv"
+	"time"
+	"sync"
 )
 
 type Robots struct {
@@ -24,6 +26,8 @@ const DISALLOW_DEF 		= "Disallow:"
 const CRAWL_DELAY_DEF	= "Crawl-Delay:"
 const SITEMAP_DEF		= "Sitemap:"
 const COMMENT_DEF		= "#"
+
+const MAX_WORKERS		= 250
 
 func (robots *Robots) NewRobots(s *bufio.Scanner) {
 	
@@ -64,20 +68,70 @@ func GetScannerFromHttpReponse(r *http.Response) *bufio.Scanner {
 	return scanner
 }
 
-func main() {
+func log1(i interface{}){
+	fmt.Println(i)
+}
 
-//	resp, err := http.Get("http://www.prezydent.pl/robots.txt")
-	resp, err := http.Get("http://allegro.pl/robots.txt")
+func log2(i interface{}){
+	fmt.Printf("%+v\n", i)
+}
+
+func getRobots(host string) *Robots{
+	
+	if has := strings.Contains(host, "http://"); !has {
+		host = "http://" + host
+	}
+	
+	resp, err := http.Get(host + "/robots.txt")
 	
 	if err != nil {
 		panic(err.Error())
 	}
 	
 	defer resp.Body.Close()
-	
+
 	r := new(Robots)
 	r.NewRobots(GetScannerFromHttpReponse(resp))
-	
-	fmt.Printf("%+v", r)
-	
+	log2(r)
+	return r
 }
+
+var domains = []string{"www.prezydent.pl","allegro.pl","stooq.pl","gazeta.pl","tvn24.pl","tvnwarszwa.pl","pudelek.pl","platforma.org","pis.org.pl","nowaprawicajkm.pl"}
+
+func worker(linkChan chan string, wg *sync.WaitGroup) {
+   defer wg.Done()
+
+   for url := range linkChan {
+     getRobots(url)
+   }
+}
+
+func paraler() {
+	lCh := make(chan string)
+    wg := new(sync.WaitGroup)
+
+    // Adding routines to workgroup and running then
+    for i := 0; i < MAX_WORKERS; i++ {
+        wg.Add(1)
+        go worker(lCh, wg)
+    }
+
+    // Processing all links by spreading them to `free` goroutines
+    for _, link := range domains {
+        lCh <- link
+    }
+
+    // Closing channel (waiting in goroutines won't continue any more)
+    close(lCh)
+
+    // Waiting for all goroutines to finish (otherwise they die as main routine dies)
+    wg.Wait()
+}
+
+func main(){
+	start := time.Now()
+	synchronized()
+	elapsed := time.Since(start)
+    fmt.Printf("took %s", elapsed)
+}
+
